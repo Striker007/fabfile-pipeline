e9297ea544872d8aa423e18fbab3db044d4edeec
v.zhydchenko <v.zhydchenko@lucky-labs.com>
2018-01-10 19:51:20 +0200

    inital

---
+++ pipelines/fabfile.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import os
+
+from fabric.api import (
+    task, roles, execute
+)
+
+from utils.main import (
+    timestamp, dir_create_remote_structure, upload_unpack_code, switch_code, pack_code
+)
+
+
+@roles('all')
+def rsync_code():
+    pass
+
+
+@task()
+@roles('proxy')
+def deploy_proxy():
+    print('deploy proxy')
+    execute(dir_create_remote_structure, REMOTE_RELEASE_DIR)
+    execute(upload_unpack_code, ARCHIVE_PATH, REMOTE_RELEASE_DIR)
+    execute(switch_code, REMOTE_RELEASE_DIR, REMOTE_CURRENT_RELEASE_LINK)
+
+
+@task(default=True)
+def deploy_main():
+    execute(deploy_proxy)
+
+
+# COMMON
+"""Global Variables:"""
+ARCHIVE_PATH = os.path.join(os.curdir, "go_pipe_deploy.tar")
+CODE_DIR = local_current_release_dir = os.pardir
+REMOTE_DIR = "/data/ggg"
+REMOTE_RELEASE_DIR = os.path.join(REMOTE_DIR, "releases", str(timestamp))
+REMOTE_CURRENT_RELEASE_LINK = os.path.join(REMOTE_DIR, "current")
+
+# config = load_yaml_config(env.deploy_config, env.environment)
+pack_code(CODE_DIR, ARCHIVE_PATH)
+
+
+if __name__ == '__main__':
+    pass
---
+++ pipelines/utils/main.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import logging
+
+from fabric.api import (
+    local, run, sudo, cd, put
+)
+
+
+"""Global Variables:"""
+timestamp = int(time.time())
+logging.basicConfig(level=logging.INFO)
+checkout_root = "src"
+
+
+def link_create(src, dst):
+    sudo("rm -rf {0}".format(dst))
+    sudo("ln -s {src} {dst}".format(src=src, dst=dst))
+
+
+def dir_create_remote_structure(working_dir):
+    print("dir create remote structure")
+    run("mkdir -p {working_dir}".format(working_dir=working_dir))
+    return working_dir
+
+
+def pack_code(src, dst):
+    if src != "" and dst != "":
+        print("pack code")
+        with cd(src):
+            local("rm -f {0}".format(dst))
+            local("tar -cf {dst} --exclude='.git*' --exclude='fabfile-pipeline' -C {src} .".format(src=src, dst=dst))
+
+
+def upload_unpack_code(local_path_to_archive, remote_dir):
+    print("upload code")
+    put(local_path_to_archive, remote_dir)
+    with cd(remote_dir):
+        arch_name = os.path.basename(local_path_to_archive)
+        run("tar -xf {0}".format(arch_name))
+        run("rm -f {0}".format(arch_name))
+
+
+def switch_code(release_dir, dir_current_release):
+    print("sw code")
+    link_create(release_dir, dir_current_release)
